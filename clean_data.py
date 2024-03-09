c490f5c78fe83cdef0450f5a627721a96ba63ef0
Jesus A. Gonzalez <jagonzalez68@gmail.com>
2024-03-08 21:30:04 -0600

    Added code to clean data, updated requirements.

---
+++ clean_data.py
@@ -0,0 +1,4 @@
+# Which columns will you use?
+# Clean your columns
+# Concatenate the columns needed for your embedding
+# Create new column with concatenated and clean text
---
+++ create_dataset.py
@@ -0,0 +1,15 @@
+import zipfile
+import pandas as pd
+import kaggle
+
+
+kaggle.api.authenticate()
+dataset = "harshitshankhdhar/imdb-dataset-of-top-1000-movies-and-tv-shows"
+kaggle.api.dataset_download_files(dataset)
+
+with zipfile.ZipFile("imdb-dataset-of-top-1000-movies-and-tv-shows.zip", 'r') as zip_ref:
+    zip_ref.extractall(".")
+
+movies = pd.read_csv("imdb_top_1000.csv")
+print(movies.columns)
+print(movies[["Series_Title", "Overview"]].head(10))
